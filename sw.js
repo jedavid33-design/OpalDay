a664fe1f1d57503c16432cdb7836ea216cdc53e3
jedavid33-design <jedavid33@gmail.com>
2026-08-22 16:48:57 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = "opalday-v0.7.1";
+const CACHE = "opalday-v0.7.2";
 const ASSETS = ["./","./index.html","./styles.css","./calendar.css","./v04.css","./app.js","./calendar.js","./notifications.js","./config.js","./manifest.webmanifest","./app-icon.png"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(fetch(event.request).then(response => {
